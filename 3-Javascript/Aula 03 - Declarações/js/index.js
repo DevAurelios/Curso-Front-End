36f6e81068d7645809c304203f05909cd2c4a0db
DevToles <DevTole@gmail.com>
2024-06-09 00:40:17 -0300

    Adicionando a Aula 3

---
+++ 3-Javascript/Aula 03 - Declarações/js/index.js
@@ -0,0 +1,22 @@
+document.getElementById("texto").innerHTML=
+"Meu primeiro texto <b>JavaScript</b>!";//Primeira maneira de uma saida de dados
+
+alert("Oi, isso é um alerta")//Terceira maneira de uma saida de dados
+
+console.log("Oi, isso é um console.log")//Quarta maneira de uma saida de dados
+
+let a,b,c;
+a=5;
+b=6;
+c=7;
+
+let pessoas = "Dimitri";//os dois vão rodar da mesma forma
+let pessoa="Dimitri";//recomendo vc deixar tudo grudado(até pq outras linguagens só usam tudo grudado)
+
+function minhafuncao(){//todas as declarações abaixo fazem parte desse bloco
+    document.write("oi <u>escrito com</u> document.write");
+    document.write("oi <u>escrito com</u> document.write");
+    document.write("oi <u>escrito com</u> document.write");
+    document.write("oi <u>escrito com</u> document.write");
+    document.write("oi <u>escrito com</u> document.write");
+}
